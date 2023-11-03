c9f6d648e8e13e45bfebfadeb42b7cc29da28a7c
jmasterr <jaim.toledo10@gmail.com>
2023-11-03 12:40:58 -0500

    updating models

--- models/Cart.js
+++ models/Cart.js
@@ -5,7 +5,8 @@ const cartSchema = new Schema(
         menu_item: [{type: Schema.Types.ObjectId, ref:"Menu", required: true}],
         total_price: {type: Number, required: true},
 
-    }
+    },
+    { timestamps: true },
 )
 
 module.exports = cartSchema
