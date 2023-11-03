e6269ce2eaa62b84ff800c2cd3b87296652319c7
williamrogerschi <william.rogers.chi@gmail.com>
2023-11-03 14:37:49 -0500

    committing some seeds

--- seed/toppingsSeed.js
+++ seed/toppingsSeed.js
@@ -1,12 +1,86 @@
 const db = require('../db/index')
 
-const { Cart, Cheese, Menu, Topping, User} = require('../models/Index.js')
+const { Topping } = require('../models/Index.js')
 
 db.on('error', console.error.bind(console, `MongoDB connection error:`))
 
 
 const pizzaToppings = [
     {
-        name: 'Pepperoni'
-    }
+        name: 'Pepperoni',
+        type: 'Meat',
+    },
+    {
+        name: 'Canadian Bacon',
+        type: 'Meat',
+      },
+      {
+        name: 'Prosciutto',
+        type: 'Meat',
+      },
+      {
+        name: 'Italian Sausage',
+        type: 'Meat',
+      },
+      {
+        name: 'Bacon',
+        type: 'Meat',
+      },
+      {
+        name: 'Ham',
+        type: 'Meat',
+      },
+      {
+        name: 'Mushrooms',
+        type: 'Veggie',
+      },
+      {
+        name: 'Green Peppers',
+        type: 'Veggie',
+      },
+      {
+        name: 'Onions',
+        type: 'Veggie',
+      },
+      {
+        name: 'Black Olives',
+        type: 'Veggie',
+      },
+      {
+        name: 'Green Olives',
+        type: 'Veggie',
+      },
+      {
+        name: 'Tomatoes',
+        type: 'Veggie',
+      },
+      {
+        name: 'Spinach',
+        type: 'Veggie',
+      },
+      {
+        name: 'Broccoli',
+        type: 'Veggie',
+      },
+      {
+        name: 'Artichoke Hearts',
+        type: 'Veggie',
+      },
+      {
+        name: 'Red Peppers',
+        type: 'Veggie',
+      },
 ]
+
+const createAndSaveToppings = async (name, type) => {
+    const newTopping = new Topping({ name, type })
+    await newTopping.save()
+}
+
+const main = async () => {
+    for (const toppings of pizzaToppings) {
+        await createAndSaveToppings(toppings.name, toppings.type)
+    }
+    db.close()
+}
+main()
