4e0af65d0824b45eada2a84c09faafd90c2d2514
jmasterr <jaim.toledo10@gmail.com>
2023-11-06 13:13:38 -0600

    created orderSeed

--- seed/orderSeed.js
+++ seed/orderSeed.js
@@ -4,44 +4,26 @@ const { Menu, Topping, Cheese, Order } = require('../models/Index')
 
 db.on('error', console.error.bind(console, `MongoDB connection error:`))
 
-const menu_info = Menu.find() 
-const toppings_info = Topping.find()
-const cheese_info = Cheese.find()
-const order_info = Order.find()
+// const menu_info = Menu.find() 
+// const toppings_info = Topping.find()
+// const cheese_info = Cheese.find()
+// const order_info = Order.find()
 
 const findMenuItemByName = async (pizzaName) => {
-    return (await Menu.find({name:pizzaName}))._id
+    return (await Menu.findOne({name:pizzaName}))._id
 }
 
 const findToppingItemByName = async (toppingName) => {
-    return (await Order.find({name:toppingName}))._id
+    console.log(toppingName)
+    console.log((await Topping.findOne({name:toppingName})))
+    return (await Topping.findOne({name:toppingName}))._id
 }
 
 const findCheesebyName = async (cheeseName) => {
-    return (await Cheese.find({name:cheeseName}))._id
+    console.log((await Cheese.findOne({name: cheeseName})._id))
+    console.log(cheeseName)
+    return (await Cheese.findOne({name:cheeseName}))._id
     }
-
-const orders = [
-    {
-        menu_item: [await findMenuItemByName("Pesto"),await findMenuItemByName("Cheese")],
-        custom_pizza: [{
-            base_pizza: await findMenuItemByName("Cheese"),
-            toppings: [await findToppingItemByName("pepperoni"),await findToppingItemByName("bacon"),await findToppingItemByName("Italian Sausage")],
-            cheeses: await findCheesebyName("asiago")
-        }],
-        total_price: "$58"
-    },
-    {
-        menu_item: [await findMenuItemByName("Margarita")],
-        custom_pizza: [{
-          base_pizza: await findMenuItemByName("Margarita"),
-          toppings: [await findToppingItemByName("ham")],
-          cheeses: await findCheesebyName("mozzarella")
-        }],
-        total_price: "$24"
-      }
-]
-
 
 const createAndSaveOrder = async (menu_item, custom_pizza, total_price) => {
     const newOrder = new Order({ menu_item, custom_pizza, total_price })
@@ -49,6 +31,27 @@ const createAndSaveOrder = async (menu_item, custom_pizza, total_price) => {
 }
 
 const main = async () => {
+    const orders = [
+        {
+            menu_item: await findMenuItemByName("Pesto"),
+            custom_pizza: [{
+                base_pizza: await findMenuItemByName("Cheese"),
+                toppings: [await findToppingItemByName("Pepperoni"),await findToppingItemByName("Bacon"),await findToppingItemByName("Italian Sausage")],
+                cheeses: await findCheesebyName("Asiago")
+            }],
+            total_price: "$58"
+        },
+        {
+            menu_item: [await findMenuItemByName("Margarita")],
+            custom_pizza: [{
+              base_pizza: await findMenuItemByName("Margarita"),
+              toppings: [await findToppingItemByName("Ham")],
+              cheeses: await findCheesebyName("Mozzarella")
+            }],
+            total_price: "$24"
+          }
+    ]
+    
     for (const order of orders) {
         await createAndSaveOrder(order.menu_item, order.custom_pizza, order.total_price)
     }
