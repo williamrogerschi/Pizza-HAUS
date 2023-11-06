a9ddd89adf92d3951669c5a7c29d7a8d9787b1d3
williamrogerschi <william.rogers.chi@gmail.com>
2023-11-06 13:33:36 -0600

    Nav bar css

--- Xmern-haus/src/components/Nav.jsx
+++ Xmern-haus/src/components/Nav.jsx
@@ -2,10 +2,15 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
 
+    let height
+
     return <div className="navbar">
+        
+        <img height='120px' width='120px' src='mern_logo_black.png' alt='pizza-logo'/>
         <Link to="/"> Home </Link>
         <Link to="/Pizza"> Pizzas </Link>
         <Link to="/CYOP"> CYOP </Link>
+
     </div>
 }
 
