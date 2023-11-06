b720747cfeba802470ffedbb0020d9d5ec68b858
williamrogerschi <william.rogers.chi@gmail.com>
2023-11-06 10:06:10 -0600

    committing some CSS

--- Xmern-haus/src/components/Header.jsx
+++ Xmern-haus/src/components/Header.jsx
@@ -1,8 +1,44 @@
+import { useLocation } from 'react-router-dom'
+
 
 const Header = () => {
+
+    const location = useLocation()
+
+
+    let headerImage
+    let headerImageStyle = {}
+
+    if( location.pathname === '/') {
+        headerImage = 'pizza-header.jpg'
+        headerImageStyle = {
+            height: '50vh',
+            width: '100%',
+            maxWidth: '100%',
+            backgroundSize: 'contain',
+            backgroundPosition: '40% 60%',
+            borderBottom: '1px solid black',
+            overflow: 'hidden'
+        }
+    } else if (location.pathname === '/CYOP') {
+        headerImage = 'CYOP.jpg'
+        headerImageStyle = {
+            height: '50vh',
+            width: '100%',
+            maxWidth: '100%',
+            backgroundSize: 'contain',
+            backgroundPosition: '40% 60%',
+            // border-bottom: '1px solid black',
+            overflow: 'hidden'
+        }
+    } else {
+        null
+    }
+
+
     return (
         <div className="header">
-            <h1>I am a header!</h1>
+            <img src={headerImage} alt='Header'  style={headerImageStyle}/>
         </div>
     )
 }
