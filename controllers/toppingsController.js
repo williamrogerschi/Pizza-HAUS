426824854355a5a6d9a08df8b79c029fa58a9295
jmasterr <jaim.toledo10@gmail.com>
2023-11-06 13:53:48 -0600

    created all the controllers

--- controllers/toppingsController.js
+++ controllers/toppingsController.js
@@ -1,15 +1,71 @@
-const { Topping } = require('../models/Index.js')
+const { Topping } = require('../models/Index');
+
+const getAllToppings = async (req, res) => {
+    try {
+        const toppings = await Topping.find()
+        res.json(toppings)
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+async function getOneTopping(req, res) {
+    try {
+        const id = req.params.id
+        const topping = await Topping.findById(id)
+        if (topping) {
+            return res.json(user)
+        }
+        return res.status(404).send('The topping with this id doesnt exist')
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+async function createNewTopping(req,res) {
+    try {
+        const topping = await new Topping (req.body)
+        await topping.save()
+        return res.status(201).json({
+            topping
+        })
+    } catch (e) {
+        return res.status(500).json({error: e.message})
+    }
+}
+
+
+async function updateTopping(req,res) {
+    try {
+        const id = req.params.id
+        const topping = await Topping.findByIdAndUpdate(id, req.body, {new: true})
+        if (topping) {
+            return res.status(200).json(topping)
+        }
+        throw new Error('topping not found')
+    } catch (e) {
+        return res.status(500).json({error: e.message})
+    }
+}
+
+async function deleteTopping(req,res) {
+    try {
+        const id = req.params.id
+        const topping =  await Topping.findByIdAndDelete(id)
+        if (topping) {
+            return res.status(200).json(topping)
+        }
+        throw new Error('topping not found')
+    } catch (e) {
+        return res.status(500).json({error: e.message})
+    }
+}
 
 
 module.exports = {
     getAllToppings,
-}
-
-async function getAllToppings (req, res) {
-    try {
-        const toppings = await Topping.find()
-        res.status(200).json(toppings)
-    } catch(e) {
-        return res.status(500).json({ error: e.message })
-    }
+    getOneTopping,
+    createNewTopping,
+    updateTopping,
+    deleteTopping
 }
